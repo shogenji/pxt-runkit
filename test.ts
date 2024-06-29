3f920a4fbae17109a5c8fb2df1c224f4d0d417de
shogenji <ruirui@ruirui.jp>
2024-06-29 10:23:40 +0900

    復元 '2024/6/29 9:06:11 細かい修正'

--- test.ts
+++ test.ts
@@ -1,19 +1,19 @@
-// // tests go here; this will not be compiled when this package is used as an extension.
-// runkit.setFixedSpeed(256)
+// tests go here; this will not be compiled when this package is used as an extension.
+runkit.setFixedSpeed(256)
 
-// basic.forever(function () {
-//     runkit.moveForward()
-//     basic.pause(500)
+basic.forever(function () {
+    runkit.moveForward()
+    basic.pause(500)
 
-//     runkit.rotateCw()
-//     basic.pause(1000)
+    runkit.rotateCw()
+    basic.pause(1000)
 
-//     runkit.moveBackward()
-//     basic.pause(500)
+    runkit.moveBackward()
+    basic.pause(500)
 
-//     runkit.rotateCcw()
-//     basic.pause(1000)
+    runkit.rotateCcw()
+    basic.pause(1000)
 
-//     runkit.stop()
-//     basic.pause(1000)
-// })
+    runkit.stop()
+    basic.pause(1000)
+})
